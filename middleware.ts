271c05d59c2246480cf3ccfc358ce21553eeef05
Shreyas Jamkhandi <shreyxs@gmail.com>
2025-07-27 17:02:49 +0530

    pwa fix

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,16 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher([
-  "/",
-  "/api/habits(.*)",
-]);
+const isProtectedRoute = createRouteMatcher(["/api/habits(.*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
-  if (isProtectedRoute(req)) {
-    await auth.protect();
-  }
+	if (isProtectedRoute(req)) {
+		await auth.protect();
+	}
 });
 
 export const config = {
-  matcher: [
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    "/(api|trpc)(.*)",
-  ],
+	matcher: [
+		"/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+		"/(api|trpc)(.*)",
+	],
 };
